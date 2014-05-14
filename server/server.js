351076a0305c6427a6eeb1a6dc9d3c563e1a43cb
Aleksandar Banjac <abanjac84@gmail.com>
2014-05-14 21:47:47 +0200

    connection established

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
 // test
+var io = require('socket.io').listen(3000);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
