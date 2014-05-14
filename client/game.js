7c5db7307589bef469bbc0c12a76910cd0277f68
mgrilec <mihael@mgrilec.com>
2014-05-14 22:44:20 +0200

    Added client update.

--- client/game.js
+++ client/game.js
@@ -1,17 +1,21 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
     //preload: preload,
     create: create,
-    //update: update,
+    update: update,
     //render: render
 });
 
+var socket;
+
 function create() {
-	var socket = io.connect('http://localhost:3000');
+	socket = io.connect('http://localhost:3000');
 	  socket.on('server_update', function (data) {
 	    console.log(data);
 	  });
 
-	  socket.emit('client_update', { id: "me" });
-
 	  console.log("hi");
+}
+
+function update() {
+	socket.emit('client_update', { id: "me" });
 }
