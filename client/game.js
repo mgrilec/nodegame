414cebc9a1ce9e67e08bbb37699fc1ddbf65f44f
mgrilec <mihael@mgrilec.com>
2014-05-15 18:46:18 +0200

    Added name input.

--- client/game.js
+++ client/game.js
@@ -6,32 +6,35 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', {
 });
 
 var socket;
+var name;
 
 function create() {
 	socket = io.connect('http://localhost:3000');
 	socket.on('server_update', function (data) {
 		console.log(data);
 	});
+
+	name = prompt('enter name');
 }
 
 function update() {
 	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
 	{
-		socket.emit('client_update', { id: 'me', key: 'left' });
+		socket.emit('client_update', { id: name, key: 'left' });
 	}
 
 	if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
 	{
-		socket.emit('client_update', { id: 'me', key: 'right' });
+		socket.emit('client_update', { id: name, key: 'right' });
 	}
 
 	if (game.input.keyboard.isDown(Phaser.Keyboard.UP))
 	{
-		socket.emit('client_update', { id: 'me', key: 'up' });
+		socket.emit('client_update', { id: name, key: 'up' });
 	}
 
 	if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN))
 	{
-		socket.emit('client_update', { id: 'me', key: 'down' });
+		socket.emit('client_update', { id: name, key: 'down' });
 	}
 }
