38538546c9018c8fef07c0e1741f441a58f7fb80
Aleksandar Banjac <abanjac84@gmail.com>
2014-05-15 19:32:35 +0200

    move function added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,35 @@
 var io = require('socket.io').listen(3000);
 
+var ships = {};
+
 io.sockets.on('connection', function (socket) {
 	socket.on('client_update', function(data) {
-		console.log(data);
+
+		if(!ships[data.id]) {
+			ships[data.id] = {};
+			ships[data.id].x = 0;
+			ships[data.id].y = 0;
+		}
+		
+		if(data.key == 'left') {
+			ships[data.id].y -= 5;
+		}
+		else if (data.key == 'right') {
+			ships[data.id].y += 5;
+		}
+		else if (data.key == 'up') {
+			ships[data.id].x -=5;
+		}
+		else if (data.key == 'down') {
+			ships[data.id].x += 5;
+		}
+
+
 	});
 });
 
 function update () {
-	io.sockets.emit('server_update', { hello: 'world'});
+	io.sockets.emit('server_update', {ships: ships});
 }
 
 setInterval(update, 1000);
